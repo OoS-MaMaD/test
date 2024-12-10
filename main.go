0336bdcf89f8932427ac7714505ced586022b890
OoS-MaMaD <mamadnt2@gmail.com>
2024-12-10 17:20:16 +0330

    added env

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/OoS-MaMaD/testmaster/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/OoS-MaMaD/testmaster/cmd"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	godotenv.Load()
+	fmt.Println(os.Getenv("Password"))
 	cmd.Execute()
 }
